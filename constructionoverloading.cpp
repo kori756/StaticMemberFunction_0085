3679780872afe9bc2b94828595c42aafd8ad7953
kori756 <khorirachmatr@gmail.com>
2026-05-05 18:58:29 +0700

    mengimplementasikan untuk class mahasiswa

--- constructionoverloading.cpp
+++ constructionoverloading.cpp
@@ -14,3 +14,24 @@ class mahasiswa{
           void cetak();
 };
 
+mahasiswa::mahasiswa(){
+}
+
+mahasiswa::mahasiswa(int iNim){
+  nim = iNim;
+}
+
+mahasiswa::mahasiswa(string iNama){
+  nama = iNama;
+}
+
+mahasiswa::mahasiswa(int iNim, string iNama){
+  nim = iNim;
+  nama = iNama;
+}
+
+void mahasiswa::cetak(){
+  cout << endl << "nim = "<< nim << endl;
+  cout << "nama = " << nama <<endl;
+}
+
