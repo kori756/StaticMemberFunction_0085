bfef7677302531208d03207e966ad7dd9138ff0c
kori756 <khorirachmatr@gmail.com>
2026-05-05 18:59:54 +0700

    menambahkan class buku

--- chainfunctioncall.cpp
+++ chainfunctioncall.cpp
@@ -1,3 +1,22 @@
 #include <iostream>
 using namespace std;
 
+class buku{
+  string judul;
+
+  public:
+
+  buku &setJudul(string judul){
+    this->judul= judul;
+    return *this;
+  }
+  string getjudul(){
+    return this->judul;
+  }
+};
+
+int main(){
+  buku bukunya;
+  cout << bukunya.setJudul("mtk").getjudul();
+  return 0;
+}
