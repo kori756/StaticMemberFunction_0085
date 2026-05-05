580d9d22c7ea5e7848441fbebc017fd121812c29
kori756 <khorirachmatr@gmail.com>
2026-05-05 18:54:04 +0700

    menambahkan class mahasiswa

--- constructorberparameter.cpp
+++ constructorberparameter.cpp
@@ -2,3 +2,15 @@
 #include <string>
 using namespace std;
 
+class mahasiswa {
+  public:
+        mahasiswa(int nim, string nama);
+
+};
+
+mahasiswa::mahasiswa(int nim, string nama){
+  cout <<"cons dengan parameter terpanggil" << endl;
+  cout << "nim : " << nim << endl;
+  cout << "nama: " << nama << endl;
+}
+
