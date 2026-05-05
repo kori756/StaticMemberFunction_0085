1e2733ea2bd100c5601191ee5afaa21f099cd062
kori756 <khorirachmatr@gmail.com>
2026-05-05 18:51:21 +0700

    menambahkan class angka

--- destructor.cpp
+++ destructor.cpp
@@ -1,3 +1,16 @@
 #include <iostream>
 using namespace std;
 
+class  angka
+{
+  private:
+  int *arr;
+  int panjang;
+
+  public:
+  angka(int);
+  ~angka();
+  void cetakdata();
+  void isidata();
+};
+
